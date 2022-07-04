bf3325d3710681d29750311052127acc4e42657d
edmarques11 <edmarquessantosdelima@gmail.com>
2022-07-03 21:13:42 -0300

    refactor: ajust structure database

--- banco_falencia.sql
+++ banco_falencia.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS endereco (
     cidade VARCHAR(100) NOT NULL,
     bairro VARCHAR(100) NOT NULL,
     numero INT NOT NULL,
-    estado VARCHAR(100),
+    estado CHAR(2),
     logradouro VARCHAR(100) NOT NULL,
     complemento VARCHAR(100),
     PRIMARY KEY (id)
@@ -32,16 +32,16 @@ CREATE TABLE IF NOT EXISTS cartao (
     data_validade DATE NOT NULL,
     tipo_cartao ENUM('debito', 'credito', 'multiplo') NOT NULL,
     cartao_virtual ENUM('sim','nao') NOT NULL,
-    limite NUMERIC(5,2),
+    limite NUMERIC(7,2),
     cliente_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (cliente_id) REFERENCES cliente (id),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS compras_credito (
+CREATE TABLE IF NOT EXISTS compras (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     valor_total FLOAT NOT NULL,
-    parcela INT NOT NULL,
+    parcelas INT NOT NULL,
     juros_porcentagem NUMERIC(2,2),
     estabelecimento VARCHAR(100) NOT NULL,
     data_compra DATE NOT NULL,
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS compras_credito (
 CREATE TABLE IF NOT EXISTS fatura (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     valor FLOAT NOT NULL,
-    parcela INT NOT NULL,
+    parcelas INT NOT NULL,
     data_vencimento DATE NOT NULL,
     cartao_id INT UNSIGNED NOT NULL UNIQUE,
     FOREIGN KEY (cartao_id) REFERENCES cartao (id),
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS fatura (
 CREATE TABLE IF NOT EXISTS compras_fatura (
     id_compra INT UNSIGNED NOT NULL,
     id_fatura INT UNSIGNED NOT NULL,
-    FOREIGN KEY (id_compra) REFERENCES compras_credito (id),
+    FOREIGN KEY (id_compra) REFERENCES compras (id),
     FOREIGN KEY (id_fatura) REFERENCES fatura (id),
     PRIMARY KEY (id_compra, id_fatura)
 );
@@ -77,19 +77,33 @@ CREATE TABLE IF NOT EXISTS conta (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS conta_externa_transacao (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    banco VARCHAR(100) NOT NULL,
+    tipo_pessoa ENUM('pf', 'pj') NOT NULL,
+    tipo_conta ENUM('cp', 'cc'),
+    cpf_cnpj VARCHAR(14) NOT NULL,
+    nome_titular VARCHAR(150) NOT NULL,
+    agencia_com_digito VARCHAR(30) NOT NULL UNIQUE,
+    numero_com_digito VARCHAR(50) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE IF NOT EXISTS transacoes (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    tipo_transacao ENUM('saque', 'deposito') NOT NULL,
-    valor_transacao FLOAT,
-    conta_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (conta_id) REFERENCES conta (id),
+    tipo_transacao ENUM('saque', 'deposito', 'transferencia') NOT NULL,
+    valor_transacao FLOAT NOT NULL,
+    transacao_interna ENUM('sim', 'nao') DEFAULT 'sim',
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS transacoes_conta (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     id_transacao INT UNSIGNED NOT NULL,
-    id_conta INT UNSIGNED NOT NULL,
+    id_conta INT UNSIGNED,
+    id_conta_externa_transacao INT UNSIGNED,
     FOREIGN KEY (id_transacao) REFERENCES transacoes (id),
     FOREIGN KEY (id_conta) REFERENCES conta (id),
-    PRIMARY KEY (id_transacao, id_conta)
+    FOREIGN KEY (id_conta_externa_transacao) REFERENCES conta_externa_transacao (id),
+    PRIMARY KEY (id)
 );
--- populate_banco_falencia.sql
+++ populate_banco_falencia.sql
@@ -19,16 +19,106 @@ INSERT
 VALUES
   ('Romulo', '42427189693', null, 'Carvalho', 1),
   ('Edmarques', '91637836066', null, 'Lima', 2),
-  ('Rafael', '62278426133', null, 'Ramos', 2),
-  ('Michel', '75582807705', null, 'Fonseca', 2),
+  ('Rafael', '62278426133', null, 'Ramos', 3),
+  ('Michel', '75582807705', null, 'Fonseca', 9),
   ('Lucas', '56691092791', null, 'Matos', 6),
-  ('Raquel', '34648301447', null, 'Cadete', 2),
+  ('Raquel', '34648301447', null, 'Cadete', 7),
   ('MB Labs', '04476538000128', 'Mb Labs', 'MB', 1),
-  ('Fatal Model', '71482024000172', 'Model', 'MODEL', 1),
-  ('Mova Sociedade de Emprestimo entre Pessoas', '42486450000186', 'Mova', 'Mova', 2),
-  ('Mambee', '24188486000100', 'Mambee', 'Mambee', 2);
+  ('Coder Corporation', '71482024000172', 'Coder Corporation', 'Cod3r', 8),
+  ('Mova Sociedade de Emprestimo entre Pessoas', '42486450000186', 'Mova', 'Mova', 5),
+  ('Mambee', '24188486000100', 'Mambee', 'Mambee', 4);
 
 INSERT
   INTO cartao(numero_cartao, codigo_seguranca, data_validade, tipo_cartao, cartao_virtual, limite, cliente_id)
 VALUES
-  ('5454335644570954', 394, '2024/04/30', 'multiplo', 'nao', 400, 1);
+  ('5454335644570964', 394, '2024/01/01', 'multiplo', 'nao', 400.00, 10),
+  ('5454335644570955', 134, '2024/12/11', 'multiplo', 'nao', 400, 2),
+  ('5454335644570956', 998, '2029/04/22', 'multiplo', 'sim', 50.0, 2),
+  ('5454335644570957', 409, '2025/08/11', 'debito', 'nao', null, 3),
+  ('5454335644570958', 110, '2024/11/08', 'multiplo', 'nao', 200, 4),
+  ('5454335644570959', 111, '2024/03/02', 'credito', 'nao', 670, 5),
+  ('5454335644570960', 122, '2024/04/15', 'multiplo', 'sim', 500, 6),
+  ('5454335644570961', 155, '2024/06/19', 'debito', 'nao', null, 7),
+  ('5454335644570962', 892, '2024/07/13', 'multiplo', 'nao', 110, 8),
+  ('5454335644570963', 982, '2024/05/17', 'debito', 'nao', null, 9);
+
+INSERT
+  INTO conta(tipo_pessoa, tipo_conta, agencia_com_digito, numero_com_digito, cliente_id)
+VALUES
+  ('pf', 'cc', '123456', '12345678901', 1),
+  ('pf', 'cp', '123457', '12345678902', 2),
+  ('pf', 'cc', '123458', '12345678903', 3),
+  ('pf', 'cc', '123459', '12345678904', 4),
+  ('pf', 'cp', '123460', '12345678905', 5),
+  ('pf', 'cp', '123461', '12345678906', 6),
+  ('pj', 'cc', '123462', '12345678907', 7),
+  ('pf', 'cp', '123463', '12345678908', 8);
+
+INSERT
+  INTO conta_externa_transacao(banco, tipo_pessoa, tipo_conta, cpf_cnpj, nome_titular, agencia_com_digito, numero_com_digito)
+VALUES
+  ('Banco da miseria', 'pf', 'cp', '15265536604', 'Euzébio Batista', '1235', '48324934'),
+  ('Banco da miseria', 'pf', 'cp', '84027307879', 'João Carlos', '9821', '578203481'),
+  ('Banco da miseria', 'pf', 'cp', '74586583126', 'Pedro Campello', '09324', '7480238432'),
+  ('Banco da miseria', 'pf', 'cp', '14157236807', 'Alice Montano', '123998', '12341094'),
+  ('Banco da miseria', 'pf', 'cp', '32961131693', 'Carla Bianca', '83242323', '22341134'),
+  ('Banco da miseria', 'pj', 'cc', '22631237000103', 'Alvaro Bezerra', '10340', '99028432'),
+  ('Banco da miseria', 'pj', 'cc', '77347267000184', 'Luana Luz', '28193', '12342564'),
+  ('Banco da miseria', 'pj', 'cc', '47432257000186', 'Sophia Andrade', '4324333', '1346998273');
+
+INSERT
+  INTO transacoes(tipo_transacao, valor_transacao)
+VALUES
+  ('deposito', 250),
+  ('saque', 255),
+  ('saque', 260),
+  ('deposito', 290),
+  ('saque', 120),
+  ('deposito', 200),
+  ('deposito', 250),
+  ('saque', 500),
+  ('deposito', 250),
+  ('transferencia', 255),
+  ('transferencia', 260),
+  ('transferencia', 290),
+  ('transferencia', 120),
+  ('transferencia', 200),
+  ('transferencia', 250),
+  ('transferencia', 500);
+
+INSERT
+  INTO transacoes_conta(id_transacao, id_conta)
+VALUES
+  (1, 1),
+  (1, 2),
+  (2, 2),
+  (3, 4),
+  (4, 5),
+  (5, 5),
+  (5, 6),
+  (8, 7);
+
+INSERT
+  INTO transacoes_conta(id_transacao, id_conta_externa_transacao)
+VALUES
+  (1, 1),
+  (1, 2),
+  (2, 2),
+  (3, 4),
+  (4, 5),
+  (5, 7),
+  (5, 6);
+
+INSERT
+  INTO compras(valor_total, parcelas, juros_porcentagem, estabelecimento, data_compra)
+VALUES
+  (20, 1, 0, 'Budega do paulo', '2024/01/28'),
+  (10, 2, 1, 'Supermercado Barra dura', '2024/01/29'),
+  (80, 1, 0, 'Posto petroleo', '2024/02/28'),
+  (120, 1, 0, 'Bar da esquina', '2024/03/28'),
+  (20, 1, 0, 'Quitanga do paulo', '2024/01/12'),
+  (200, 1, 0, 'Bar do João', '2024/01/28'),
+  (20, 1, 0, 'Sorveteria preta', '2024/12/11'),
+  (20, 1, 0, 'Açaí do nordeste', '2024/01/15'),
+  (20, 1, 0, 'Cabana dance', '2024/01/14'),
+  (20, 1, 0, 'Cinema olho vivo', '2024/05/09');
